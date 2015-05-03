5fae80be778f5662991a7e7dfe01faa933899b3c
Vader-San <vadersan@gmail.com>
2015-05-03 13:12:43 +0200

    start server only

--- run.sh
+++ run.sh
@@ -3,13 +3,6 @@
 set -e
 
 asadmin start-domain
-echo "=> Deploying App"
-asadmin deploy /opt/glassfish4/bin/MTE.war
-echo "=> Enabling secure admin login"
-/enable_secure_admin.sh
-echo "=> Restarting Glassfish server"
-asadmin stop-domain
-echo "=> Starting and running Glassfish server"
-asadmin start-domain -w
+
 
 
